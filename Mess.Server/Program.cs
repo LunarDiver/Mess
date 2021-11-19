515d78951deed39aaf17abbda9dca1d48d5c14fa
Luna <dev@lunardiver.com>
2021-11-19 20:41:47 +0100

    Added closing with error code if args can't be parsed.

--- Mess.Server/Program.cs
+++ Mess.Server/Program.cs
@@ -5,4 +5,7 @@ using Mess.Server;
 AppArguments parsedArgs = null;
 Parser.Default.ParseArguments<AppArguments>(args).WithParsed(parsed => parsedArgs = parsed);
 
+if(parsedArgs == null)
+    Environment.Exit(1);
+
 Console.WriteLine(JsonSerializer.Serialize(parsedArgs));
