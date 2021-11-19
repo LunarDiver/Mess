ab7c209685ae4526af2cdbfe437c85a604ff866f
Luna <dev@lunardiver.com>
2021-11-19 21:46:06 +0100

    Added proof-of-concept server code.

--- Mess.Server/Program.cs
+++ Mess.Server/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Net.Sockets;
+using System.Text.Json;
 using CommandLine;
 using Mess.Server;
 
@@ -9,3 +10,33 @@ if(parsedArgs == null)
     Environment.Exit(1);
 
 Console.WriteLine(JsonSerializer.Serialize(parsedArgs));
+
+var listener = TcpListener.Create(parsedArgs.ListeningPort);
+listener.Start(0xff);
+
+var clients = new List<TcpClient>();
+
+while(true)
+{
+    if(listener.Pending())
+        clients.Add(await listener.AcceptTcpClientAsync());
+
+    foreach(TcpClient client in clients)
+    {
+        if(client.Available > 0)
+        {
+            byte[] data = new byte[client.Available];
+            client.GetStream().Read(data);
+
+            clients.AsParallel().ForAll(cl =>
+            {
+                if(cl == client)
+                    return;
+
+                cl.GetStream().Write(data);
+            });
+        }
+    }
+
+    await Task.Delay(1000);
+}
