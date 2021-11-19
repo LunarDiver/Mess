26d02ad2104f1902ccdd874ada741773b3b989a1
Luna <dev@lunardiver.com>
2021-11-19 20:33:02 +0100

    Added Mess.Server project with basic app args.

---
+++ Mess.Server/AppArguments.cs
@@ -0,0 +1,7 @@
+﻿namespace Mess.Server;
+
+public class AppArguments
+{
+    [CommandLine.Option('p', "port", Required = true, HelpText = "Specifies the port the server will listen to.")]
+    public ushort ListeningPort { get; set; }
+}
---
+++ Mess.Server/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json;
+using CommandLine;
+using Mess.Server;
+
+AppArguments parsedArgs = null;
+Parser.Default.ParseArguments<AppArguments>(args).WithParsed(parsed => parsedArgs = parsed);
+
+Console.WriteLine(JsonSerializer.Serialize(parsedArgs));
